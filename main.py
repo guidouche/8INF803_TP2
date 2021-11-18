b5ba19b2aded06fdf547e1c9a130cd1decc2ee51
Loic Weiss <loic@MacBook-Pro-de-Loic.local>
2021-11-18 10:24:03 -0500

    add - starting of project

--- main.py
+++ main.py
@@ -1,7 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+import re
 
-def print_hi(name):
-    print(f'Hi, {name}')
+HTML_PARSER = "html.parser"
+
+
+class Parsing:
+    def __init__(self):
+        self.counter_error = 0
+        self.spell_found = False
+        self.counter_spell_not_displayed = 0
+        self.base_url = "https://aonprd.com/"
+
+    def init_soup(self, url, is_from_web):
+
+        if is_from_web:
+            page = requests.get(self.base_url + url)
+            soup = BeautifulSoup(page.content, HTML_PARSER)
+        else:
+            soup = BeautifulSoup(url, HTML_PARSER)
+
+        return soup
+
+    def convert_result_soup_to_string(self, soup, tag):
+
+        #spellListHtmlPage = BeautifulSoup(str(self.base_url + url), HTML_PARSER)
+        spellDisplayDiv = soup.findAll(tag)
+        paragraphs = []
+        for x in spellDisplayDiv:
+            paragraphs.append(str(x))
+
+        str1 = ''.join(paragraphs)
+
+
+        return str1
+
+
+    def get_spell_list(self, soup):
+        spell_list = {}
+        div = soup.findAll('td')
+
+        paragraphs = []
+        for x in div:
+            paragraphs.append(str(x))
+
+        str1 = ''.join(paragraphs)
+
+        #print(str1)
+
+        spellDiv = re.search(r"(.*<h3 class=\"framing\")>(Offense<\/h3>.*<h3 class=\"framing\">Statistics<\/h3>)", str1)
+
+        #print(spellDiv.group(2))
+
+        #print(spellDiv)
+        spellListDiv = re.search(r"<b>Spells Prepared <\/b>((.|\n)*)+", str(spellDiv.group()))
+        #print(spellListDiv.group())
+        spellList = re.findall(r"(?<=<i>)((\w|\d|\n|[’]| )+?)(?=<\/i>)", str(spellListDiv.group()))
+
+        for spell in spellList:
+            print(spell[0])
+        #print(spellList[0][0])
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    pars = Parsing()
+    """
+    spellListHtmlPage = BeautifulSoup(pars.convert_result_soup_to_string(pars.init_soup("Monsters.aspx?Letter=All", True), "td"), HTML_PARSER)
+    spellDisplayDiv = spellListHtmlPage.findAll('a')
+    t = pars.convert_result_soup_to_string(pars.init_soup(pars.convert_result_soup_to_string(spellListHtmlPage, 'a'), False), 'a')
+    """
+
+    print(pars.get_spell_list(pars.init_soup("MonsterDisplay.aspx?ItemName=Solar", True)))
+
+
+    """t = t.split("\"")
+    for u in t:
+        if "MonsterDisplay" in u:
+            print(u)"""
+    #spellName = spellDisplayDiv.get("td")
