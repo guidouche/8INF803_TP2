8e7239f51534671edc5f63f72709ba8ced2dd883
Loic Weiss <loic@MacBook-Pro-de-Loic.local>
2021-11-30 18:16:55 -0500

    edmond t'es vraiment le meilleur prof, no joke

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import json
 import os
 
+import pyspark
 import requests
 from bs4 import BeautifulSoup
 import re
+
+from pyspark import *
+from pyspark.shell import spark
 
 from MonsterClass import Monster
 
@@ -49,7 +53,7 @@ class Parsing:
 
         str1 = ''.join(paragraphs)
 
-        #print(str1)
+        # print(str1)
 
         # (.*<h3 class=\"framing\")>(Offense<\/h3>.*<h3 class=\"framing\">Statistics<\/h3>)
         spellDiv = re.search(r"(.*<h3 class=\"framing\")>(Offense<\/h3>((.|\n)*)<h3 class=\"framing\">Statistics)",
@@ -62,7 +66,7 @@ class Parsing:
             # <b>Spells Prepared <\/b>((.|\n)*)+
             # (<b>Spells*.*<\/b>((.|\n)*)+)
             spellListDiv = re.search(r"(<b.*Spells*.*<\/b>((.|\n)*)+)", str1)
-            #print(spellListDiv.group())
+            # print(spellListDiv.group())
 
             spellList = re.findall(r"(?<=<i>)((\w|\d|\n|[’]| )+?)(?=<\/i>)", str(spellListDiv.group()))
 
@@ -71,8 +75,8 @@ class Parsing:
             #    print(spell[0])
         # print(spellList[0][0])
 
-def fill_file(spell):
 
+def fill_file(spell):
     a = []
     filename = 'myFile.json'
 
@@ -88,56 +92,65 @@ def fill_file(spell):
         with open(filename, mode='w') as f:
             f.write(json.dumps(feeds, indent=2))
 
+
 if __name__ == '__main__':
-
-    pars = Parsing()
-
-    """
-    spellListHtmlPage = BeautifulSoup(
-        pars.convert_result_soup_to_string(pars.init_soup("Monsters.aspx?Letter=All", True), "td"), HTML_PARSER)
-    spellDisplayDiv = spellListHtmlPage.findAll('a')
-    t = pars.convert_result_soup_to_string(
-        pars.init_soup(pars.convert_result_soup_to_string(spellListHtmlPage, 'a'), False), 'a')
-
+    # pars = Parsing()
     #
-    """
+    # spellListHtmlPage = BeautifulSoup(
+    #      pars.convert_result_soup_to_string(pars.init_soup("Monsters.aspx?Letter=All", True), "td"), HTML_PARSER)
+    # spellDisplayDiv = spellListHtmlPage.findAll('a')
+    # t = pars.convert_result_soup_to_string(
+    #      pars.init_soup(pars.convert_result_soup_to_string(spellListHtmlPage, 'a'), False), 'a')
+    #
+    #
+    #
+    #
+    # arrayMonster = []
+    #
+    # t = t.split("\"")
+    # for u in t:
+    #     if "MonsterDisplay" in u:
+    #         arrayMonster.append(Monster(u))
+    # # arrayMonster.append(Monster("MonsterDisplay.aspx?ItemName=Solar"))
+    # counter = 0
+    #
+    # for j in arrayMonster:
+    #     #if counter > 10:
+    #     #    break
+    #     print(j.url)
+    #
+    #     counter += 1
+    #     tmpSpellsList = pars.get_spell_list(pars.init_soup(j.url, True))
+    #
+    #     if tmpSpellsList is not None:
+    #         for spell in tmpSpellsList:
+    #             if spell[0][-1] == ' ':
+    #                 my_str = spell[0][:-1]
+    #             else:
+    #                 my_str = spell[0]
+    #             j.spells.append(my_str)
+    #     j.spells = list(dict.fromkeys(j.spells))
+    #
+    #
+    #     myDict = {
+    #         "name": j.name,
+    #         "spells": j.spells
+    #     }
+    #
+    #     fill_file(myDict)
+    #     print(myDict)
 
-    arrayMonster = []
+# df = spark.read.json("./myFile.json")
+#
+    multiline_df = spark.read.option("multiline", "true") \
+     .json("./myFile.json")
+#
+    multiline_df.show()
 
-    #t = t.split("\"")
-    #for u in t:
-        #if "MonsterDisplay" in u:
-            #arrayMonster.append(Monster(u))
-    #arrayMonster.append(Monster("MonsterDisplay.aspx?ItemName=Solar"))
-    counter = 0
+    aggr_rdd = multiline_df.rdd.flatMap(lambda t: [(i.lower(), t[0]) for i in t[1]]) #.collect()#.map(lambda file, word: (word, [file]))
+    inverted_index = aggr_rdd.groupByKey().mapValues(list).collect()
 
-    for j in arrayMonster:
-        if counter > 10:
-           break
-        print(j.url)
-
-        counter += 1
-        tmpSpellsList = pars.get_spell_list(pars.init_soup(j.url, True))
-
-        if tmpSpellsList is not None:
-            for spell in tmpSpellsList:
-                if spell[0][-1] == ' ':
-                    my_str = spell[0][:-1]
-                else:
-                    my_str = spell[0]
-                j.spells.append(my_str)
-
-        #print(j.spells)
-        j.spells = list(dict.fromkeys(j.spells))
-
-
-        myDict = {
-                "name": j.name,
-                "spells": j.spells
-            }
-
-        fill_file(myDict)
-        print(myDict)
+    print(inverted_index[0][0])
 
 # print(pars.get_spell_list(pars.init_soup("MonsterDisplay.aspx?ItemName=Solar", True)))
 # print(pars.get_spell_list(pars.init_soup("MonsterDisplay.aspx?ItemName=Astomoi", True)))
